5b6657ecd5d0bef2bccb507b00aa448680dc7842
Jose Sanchez <engineer@copydataai.com>
2024-08-06 14:56:29 +0100

    fix: format

--- astro.config.ts
+++ astro.config.ts
@@ -1,32 +1,35 @@
-import { defineConfig } from 'astro/config';
-import remarkToc from 'remark-toc';
+import { defineConfig } from "astro/config";
+import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  markdown: {
-    remarkPlugins: [
-      remarkToc,
-      [
-        remarkCollapse,
-        {
-          test: "Table of contents",
-        },
-      ],
-    ],
-    shikiConfig: {
-      // For more themes, visit https://shiki.style/themes
-      themes: { light: "min-light", dark: "night-owl" },
-      wrap: true,
-    },
-  },
-  vite: {
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
-  },
-  integrations: [tailwind({
-          applyBaseStyles: false,
-  }), react()]
+	markdown: {
+		remarkPlugins: [
+			remarkToc,
+			[
+				remarkCollapse,
+				{
+					test: "Table of contents",
+				},
+			],
+		],
+		shikiConfig: {
+			// For more themes, visit https://shiki.style/themes
+			themes: { light: "min-light", dark: "night-owl" },
+			wrap: true,
+		},
+	},
+	vite: {
+		optimizeDeps: {
+			exclude: ["@resvg/resvg-js"],
+		},
+	},
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		react(),
+	],
 });
--- src/config.ts
+++ src/config.ts
@@ -1,51 +1,50 @@
 import type { Site } from "./types";
 
 export const SITE: Site = {
-    website: "https://copydataai.com/",
-    author: "Jose Sanchez",
-    desc: "A Collection of Blogs related to my Journey",
-    title: "Thriving greatness",
-    ogImage: "",
-    lightAndDarkMode: true,
-    postPerPage: 5,
+	website: "https://copydataai.com/",
+	author: "Jose Sanchez",
+	desc: "A Collection of Blogs related to my Journey",
+	title: "Thriving greatness",
+	ogImage: "",
+	lightAndDarkMode: true,
+	postPerPage: 5,
 };
 
 export const LOCALE = {
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+	lang: "en", // html lang code. Set this empty and default will be "en"
+	langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
 } as const;
 
 export const LOGO_IMAGE = {
-    enable: false,
-    svg: true,
-    width: 80,
-    height: 30,
+	enable: false,
+	svg: true,
+	width: 80,
+	height: 30,
 };
 
 export const SOCIALS = {
-    Github: {
-        name: "Github",
-        href: "https://github.com/copydataai",
-        linkTitle: `${SITE.author} on Github`,
-        active: true,
-    },
-    Linkedin: {
-        name: "Linkedin",
-        href: "https://www.linkedin.com/in/copydataai",
-        linkTitle: `${SITE.author} on LinkedIn`,
-        active: true,
-    },
-    Mail: {
-        name: "Mail",
-        href: "mailto:me@copydataai.com",
-        linkTitle: `Send an email to ${SITE.author}`,
-        active: true,
-    },
-    Twitter: {
-
-        name: "Twitter",
-        href: "https://x.com/copydataaireal",
-        linkTitle: `${SITE.author} on Twitter`,
-        active: true,
-    },
-}
+	Github: {
+		name: "Github",
+		href: "https://github.com/copydataai",
+		linkTitle: `${SITE.author} on Github`,
+		active: true,
+	},
+	Linkedin: {
+		name: "Linkedin",
+		href: "https://www.linkedin.com/in/copydataai",
+		linkTitle: `${SITE.author} on LinkedIn`,
+		active: true,
+	},
+	Mail: {
+		name: "Mail",
+		href: "mailto:me@copydataai.com",
+		linkTitle: `Send an email to ${SITE.author}`,
+		active: true,
+	},
+	Twitter: {
+		name: "Twitter",
+		href: "https://x.com/copydataaireal",
+		linkTitle: `${SITE.author} on Twitter`,
+		active: true,
+	},
+};
